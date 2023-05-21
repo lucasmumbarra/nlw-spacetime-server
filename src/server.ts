562fa4cc46db6b36abb8d724041f4ac58147e87b
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2023-05-21 01:23:13 -0300

    Feature: Added server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { resolve } from 'node:path'
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads',
+})
+
+app.register(cors, {
+    origin: true,
+})
+
+app.register(jwt, {
+    secret: 'spacetime'
+})
+
+app.listen({
+    port: 3333,
+    host: '0.0.0.0',
+}).then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+})
